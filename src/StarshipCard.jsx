929826218a3328a51ba286673f1927039c41ffd8
imani.devs <imaniroberts.dev@gmail.com>
2023-04-18 09:27:01 -0400

    add comments

--- src/StarshipCard.jsx
+++ src/StarshipCard.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'; //useState and useEffect are used to fetch the data from the API and set it to the state
 
-
-function Starships({cards}) {
+function Starships({ cards }) {
+	//cards is the prop that is being passed to the StarshipCard component.
 	return (
-		<div>
-			<button>{cards.name}</button>
-	</div>
-)
+		<div className="cards">
+			<button className="cardButton">
+				{cards.name} //cards.name is the name of the starship
+				{cards.crew} //cards.crew is the crew of the starship
+			</button>
+		</div>
+	);
 }
 
 export default Starships;
